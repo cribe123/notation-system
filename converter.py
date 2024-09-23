fbfc22611a1f09b866819c543edd2d1ec3241618
cribe123 <156344152+cribe123@users.noreply.github.com>
2024-09-23 14:20:24 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 from math import pow
-a=input('Введите число...')
-b=int(input('Введите его СС цифрой...'))
+a=input('Введите число... ')
+b=int(input('Введите его СС цифрой... '))
 xpow = 0
 for i in range(len(a)-1,-1,-1):
     xpow+=int(a[i])*pow(b,i)
