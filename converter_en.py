ac97cf498d87ba4c593ab3b9d61e28e22dbbe259
cribe123 <156344152+cribe123@users.noreply.github.com>
2024-09-23 14:15:42 +0300

    Create converter_en.py

---
+++ converter_en.py
@@ -0,0 +1,7 @@
+from math import pow
+a=input('Enter a number...')
+b=int(input('Enter its system...'))
+xpow = 0
+for i in range(len(a)-1,-1,-1):
+    xpow+=int(a[i])*pow(b,i)
+print('Number in decimal system:',int(xpow))
