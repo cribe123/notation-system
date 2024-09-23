26dd709e13bacb0ecb966dfd824ea12c384102ca
cribe123 <156344152+cribe123@users.noreply.github.com>
2024-09-23 14:20:38 +0300

    Update converter_en.py

--- converter_en.py
+++ converter_en.py
@@ -1,6 +1,6 @@
 from math import pow
-a=input('Enter a number...')
-b=int(input('Enter its system...'))
+a=input('Enter a number... ')
+b=int(input('Enter its system... '))
 xpow = 0
 for i in range(len(a)-1,-1,-1):
     xpow+=int(a[i])*pow(b,i)
