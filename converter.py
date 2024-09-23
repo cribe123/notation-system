9bec034f2fd7f6092e98a01e3be6644cbd433797
cribe123 <156344152+cribe123@users.noreply.github.com>
2024-09-23 14:13:30 +0300

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,7 @@
+from math import pow
+a=input('Введите число...')
+b=int(input('Введите его СС цифрой...'))
+xpow = 0
+for i in range(len(a)-1,-1,-1):
+    xpow+=int(a[i])*pow(b,i)
+print('Число в десятичной СС:',int(xpow))
